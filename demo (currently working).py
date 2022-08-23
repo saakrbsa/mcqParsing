8665c14107d1e39bd1072d8a5fe6fcfe8b94dafa
saakrbsa <saakrbsa@gmail.com>
2022-08-23 17:16:13 +0530

    Demo Commit

--- demo (currently working).py
+++ demo (currently working).py
@@ -50,7 +50,10 @@ for txtFile in Path("txts").glob("*.txt"):
 
 # Splits the questions into a list assuming there is no empty lines inside each question
 
-    questions = re.split(r'\n\s*\n', data)
+    frmt_1 = data.split("\n",1)[1]
+    frmt_2 = frmt_1.split("\n",1)[1]
+
+    questions = re.split(r'\n\s*\n', frmt_2)
     
     final_questions = []
     
